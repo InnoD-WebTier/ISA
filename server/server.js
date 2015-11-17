4a6e01924dbab3ed3eed496faad65689d16b762b
Jeff Gong <jgong@berkeley.edu>
2015-11-16 17:46:19 -0800

    Played around with orion tutorial, added admin packages and page

---
+++ server/server.js
@@ -0,0 +1,17 @@
+// Announcements = new Mongo.Collection("announcements");
+
+Meteor.startup(function () {
+	console.log("Successful startup!");
+	// Meteor.publish("announcements", function() {
+	// 	return Announcements.find();
+	// });
+});
+
+Meteor.methods({
+
+	// getAnnouncements: function() {
+	// 	var itemList = Announcements.find({}).fetch();
+	// 	console.log(itemList);
+	// 	return itemList;
+	// }
+});
