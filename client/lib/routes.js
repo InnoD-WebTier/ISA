de30656da64f5dc281bfdfc07a07252fb20e9aae
Julia Sun <julia.sun@berkeley.edu>
2015-11-22 23:54:32 -0800

    Restructured and added pages, styles, etc.

--- client/lib/routes.js
+++ client/lib/routes.js
@@ -1,27 +1,15 @@
-// I Tend To Think Too Deeply
-//
-// My friend was eating an orange,
-// So I asked him,
-// Is the fruit called an orange
-// Because it is the color orange?
-// Or is the color called orange,
-// Because the fruit is orange?
-//
-// He said,
-// This is a tangerine.
-//
-// #PoetryTier
-// #PoemsFromKidsIHadEnglishWith
-// #ThisIsABoringFile
-
 Router.route('/', function() {
-  this.render('landing');
+    this.render('home');
 });
 
 Router.route('/about', function() {
-  this.render('about');
+    this.render('about');
+});
+
+Router.route('/gallery', function() {
+    this.render('gallery');
 });
 
 Router.configure({
-  layoutTemplate: 'ApplicationLayout'
+    layoutTemplate: 'ApplicationLayout'
 });
