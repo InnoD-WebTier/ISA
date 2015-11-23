bce817f5bdfb891527d8c42a63faa394865d495e
Julia Sun <julia.sun@berkeley.edu>
2015-11-22 23:57:30 -0800

    More reorganizing, added routes for pages

--- client/lib/routes.js
+++ client/lib/routes.js
@@ -6,6 +6,14 @@ Router.route('/about', function() {
     this.render('about');
 });
 
+Router.route('/blog', function() {
+    this.render('blog');
+});
+
+Router.route('/events', function() {
+    this.render('events');
+});
+
 Router.route('/gallery', function() {
     this.render('gallery');
 });
